26c10c7c34d919e90e0071ff5f92f98aec56f34b
Grant Smith <gsmith@tampabay.com>
2020-04-27 12:14:17 -0400

    added base logic for collider as well as a gitignore for the Temp folder

---
+++ Assets/Scripts/rayCasting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayCasting : MonoBehaviour
+{
+    //-- Sets so that you can give layers to be ignored
+    public LayerMask mask; 
+    // Update is called once per frame
+    void Update()
+    {
+        //-- This should hopefully be pretty straightforward. The inst. call for ray has a lot of overloads I should look into
+        //-- Items must have collider attached in order to be 'seen'
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 100, mask))
+        {
+
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
+    }
+}
