6bfea44b22276963dac6799750321ba38573ddbf
Grant Smith <gsmith@tampabay.com>
2020-04-29 09:27:04 -0400

    coroutine work finished. waypoint and input based coroutines added with comments

--- Assets/Scripts/colorChanging.cs
+++ Assets/Scripts/colorChanging.cs
@@ -15,23 +15,59 @@ public class colorChanging : MonoBehaviour
     int[] powersOfTwo = new int[5];
     int[,] board = new int[3, 3];
     List<string> names = new List<string>();
+    IEnumerator currentMoveCoroutine;
+    public Transform[] path;
 
     void Start () {
-        StartCoroutine(DoSomething(possibleNames, .5f));
+
+        //StartCoroutine(DoSomething(possibleNames, .5f));
+        StartCoroutine(FollowPath());
     }
 
     void Update(){
         //SpaceBarListener();
+
+        //-- scoots the block around on spacebar press. current routine is stored and checked so that it can finish 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(currentMoveCoroutine != null)
+            {
+                StopCoroutine(currentMoveCoroutine);
+            }
+            currentMoveCoroutine =  Move(Random.onUnitSphere *5, 8);
+            StartCoroutine(currentMoveCoroutine);
+        }
     }
 
-    IEnumerator  DoSomething(string[] messages, float delay)
+
+
+    IEnumerator Move(Vector3 destination, float speed)
+    {
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        //-- Takes the waypoints assigned in the view and moves the cube along them in order. 
+        foreach (Transform waypoint in path)
+        {
+            //-- yield return means that the routine will finish before starting the next waypoint.
+            //-- i think that works because it's not based on usert input
+            yield return StartCoroutine(Move(waypoint.position, 8));
+        }
+    }
+    /*IEnumerator  DoSomething(string[] messages, float delay)
     {
         foreach (string msg in messages)
         {
             print(name);
             yield return new WaitForSeconds(delay);
         }
-    }
+    }*/
 
     public void OnClickRenderColor () {
         //Enables and disables the color of the cube  - GameObject linked to cube and function linked to button
